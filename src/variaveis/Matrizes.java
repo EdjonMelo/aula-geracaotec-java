789d164442c8191275b80df2ba10f49219f6344c
Nassor Paulino da Silva <nassor@gmail.com>
2014-07-14 14:36:14 -0300

    Atualização matrizes

--- src/variaveis/Matrizes.java
+++ src/variaveis/Matrizes.java
@@ -10,7 +10,7 @@ public class Matrizes {
 	public static void main(String[] args) {
 		// inicializando
 		int[][] duasDimensoes = new int[10][10]; // como se fosse uma tabela
-		int[][] duasDimensoesFormaDois = { { 12, 22, 44 }, { 22, 44, 22 } };
+		int[][] duasDimensoesFormaDois = { { 12, 22, 44 }, { 22, 44, 22 }, {33, 15, 66} };
 //		int[][][] tresDimensoes = new int[10][10][20]; // Só vi em jogos 3d e tretas matemáticas
 //		int[][][][] quatroDimensoes = new int[2][2][2][2]; // nunca vi nem usei.
 		// ...
@@ -19,14 +19,15 @@ public class Matrizes {
 		System.out.println(duasDimensoesFormaDois[0]);
 		System.out.println(duasDimensoesFormaDois[0][2]);
 		System.out.println(duasDimensoesFormaDois[1][0]);
+		System.out.println(duasDimensoesFormaDois[2][1]);
 		
 		
 		// Navegando
-//		for(int i = 0; i < duasDimensoesFormaDois.length; i++) {
-//			for(int j=0; j < duasDimensoesFormaDois[0].length; j++) {
-//				System.out.printf("[%d][%d]: %d\n", i, j, duasDimensoesFormaDois[i][j]);;
-//			}
-//		}
+		for(int i = 0; i < duasDimensoesFormaDois.length; i++) {
+			for(int j = 0; j < duasDimensoesFormaDois[0].length; j++) {
+				System.out.printf("[%d][%d]: %d\n", i, j, duasDimensoesFormaDois[i][j]);;
+			}
+		}
 		
 	}
 
