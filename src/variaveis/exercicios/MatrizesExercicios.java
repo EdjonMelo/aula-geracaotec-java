38f848e51a21c4c297679adc7c03cf163fa35319
Nassor Paulino da Silva <nassor@gmail.com>
2014-07-15 14:47:14 -0300

    Matrizes [2]

--- src/variaveis/exercicios/MatrizesExercicios.java
+++ src/variaveis/exercicios/MatrizesExercicios.java
@@ -1,5 +1,7 @@
 package variaveis.exercicios;
 
+import java.util.Scanner;
+
 /*
  * 1 - Escreva um método em java para somar todos os números da matriz:
  * 
@@ -41,11 +43,94 @@ package variaveis.exercicios;
  */
 
 public class MatrizesExercicios {
+	
+	private static void imprimeMatriz() {
+		int[][] matriz = {{1, 2, 4, 10}, {3, 5, 1, 6}, {7, 3, 2, 9}};
+		int soma = 0;
+		
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				soma += matriz[i][j];
+			}
+		}
+		
+		System.out.println(soma);
+	}
+	
+	private static double[] analiseNumerica() {
+		double[][] matriz = {{2, 4, 5, 10}, {6, 9, 2, 1}, {3, 4, 1, 7}, {9, 8, 4, 0.3}};
+		double[] analise = {Double.MAX_VALUE, Double.MIN_VALUE, 0};
+		
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz[i].length; j++) {
+				
+				if(matriz[i][j] < analise[0]) {
+					analise[0] = matriz[i][j]; 
+				}
+				
+				if(matriz[i][j] > analise[1]) {
+					analise[1] = matriz[i][j]; 
+				}
+				
+				analise[2] += matriz[i][j];
+			} 
+		}
+		
+		analise[2] /= matriz.length * matriz[0].length;
+		
+		return analise;
+	}
+	
+	private static void armazenaNomeEmail() {
+		Scanner kb01 = new Scanner(System.in);
+		
+		System.out.print("Digite a quantidade de usuários a registar: ");
+		int len = kb01.nextInt();
+		System.out.println(len);
+		
+		String[][] nomeEmail = new String[len][2];
+		for(int i = 0; i < len; i++) {
+			for(int j = 0; j < 2; j++) {
+				nomeEmail[i][j] = "";
+			}
+		}
+		
+		for(int i = 0; i < len; i++) {
+			System.out.println(i);
+			while(nomeEmail[i][0].length() < 2 || nomeEmail[i][0].length() > 50) {
+				Scanner kb02 = new Scanner(System.in);
+				System.out.print("Digite o nome: ");
+				nomeEmail[i][0] = kb02.nextLine();
+			}
+			
+			while(nomeEmail[i][1].length() < 2 || nomeEmail[i][1].length() > 50) {
+				Scanner kb03 = new Scanner(System.in);
+				System.out.print("Digite o email: ");
+				nomeEmail[i][1] = kb03.nextLine();
+			}
+		}
+		
+		for(int i = 0; i < len; i++) {
+			for(int j = 0; j < 2; j++) {
+				System.out.println(nomeEmail[i][j]);
+			}
+		}
+		
+		
+		kb01.close();
+		
+		
+	}
 
 	public static void main(String[] args) {
-		
-		
-
+//		imprimeMatriz();
+//		
+		for(double a : analiseNumerica()) {
+			System.out.println(a);
+		}
+//		
+//		armazenaNomeEmail();
+		 
 	}
 
 }
