67d5d6f1be87a6fbe66672b046030e3dfb3892dc
Nassor Paulino da Silva <nassor@gmail.com>
2014-07-15 14:38:59 -0300

    Mapa do jogo

---
+++ src/oo/jogo/Mapa.java
@@ -0,0 +1,16 @@
+package oo.jogo;
+
+/*
+ * O Mapa possui 2 atributos:
+ * - Dimensão uma matriz de tamanho definido durante a construção da 
+ *  classe
+ * - Obstáculos, são array contendo (x posições definidas pelo construtor)
+ *  posições [x,y] definidas pela dimensão.
+ * 
+ * Os obstáculos devem ser gerados assim que o mapa for construído. 
+ * Não podendo haver um obstáculo do lado do outro.
+ * 
+ */
+public class Mapa {
+
+}
