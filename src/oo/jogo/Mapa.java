9f9b837d3dbd48a00d9925876078ac3d874b9aaf
Nassor Paulino da Silva <nassor@gmail.com>
2014-07-15 15:04:34 -0300

    Colher de chá - Mapa

--- src/oo/jogo/Mapa.java
+++ src/oo/jogo/Mapa.java
@@ -2,15 +2,26 @@ package oo.jogo;
 
 /*
  * O Mapa possui 2 atributos:
- * - Dimensão uma matriz de tamanho definido durante a construção da 
+ * - Dimensão é uma matriz de tamanho definido durante a construção da 
  *  classe
- * - Obstáculos, são array contendo (x posições definidas pelo construtor)
- *  posições [x,y] definidas pela dimensão.
+ * - Obstáculos, são array contendo, ? posições [x,y] definidas pelo 
+ *   construtor, tendo como limite as dimensões do mapa.
  * 
  * Os obstáculos devem ser gerados assim que o mapa for construído. 
  * Não podendo haver um obstáculo do lado do outro.
  * 
  */
 public class Mapa {
-
+	private int[][] dimensao;
+	private int[][] obstaculos;
+	
+	public Mapa(int largura, int altura, int numObstaculos) {
+		this.dimensao = new int[largura][altura];
+		gerarObstaculos(numObstaculos);
+	}
+	
+	private void gerarObstaculos(int numObstaculos) {
+		this.obstaculos = new int[numObstaculos][2];
+		// .... gerar
+	}
 }
