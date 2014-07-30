a02fbd06c673f4030ce697b519df02198f397405
Nassor Paulino da Silva <nassor@gmail.com>
2014-07-30 15:55:33 -0300

    Correção exercício receita

--- src/oo/exercicios/NovaEntradaDeCinema.java
+++ src/oo/exercicios/NovaEntradaDeCinema.java
@@ -6,7 +6,6 @@ public class NovaEntradaDeCinema {
 
 	public static void main(String[] args) {
 		try {
-			
 			EntradaDeCinema pokemon = new EntradaDeCinema("03/07/2000", 17.00f,
 					1, 15.00f);
 			pokemon.calculaDesconto("03/07/1988");
