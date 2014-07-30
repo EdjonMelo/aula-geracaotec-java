9b4ac7248a546672f78e19bfbc672dc7298c6063
Nassor Paulino da Silva <nassor@gmail.com>
2014-07-30 14:18:11 -0300

    DateMundoReal e Exercicio agenda

--- src/oo/exercicios/NovaEntradaDeCinema.java
+++ src/oo/exercicios/NovaEntradaDeCinema.java
@@ -1,16 +1,25 @@
 package oo.exercicios;
 
+import java.text.ParseException;
+
 public class NovaEntradaDeCinema {
 
 	public static void main(String[] args) {
-		EntradaDeCinema pokemon = new EntradaDeCinema("03/07/2000", 17.00f, 1, 15.00f);
-		pokemon.calculaDesconto("03/07/1988");
-		System.out.println(pokemon);
-		
-		
-		EntradaDeCinema oProcurado = new EntradaDeCinema("03/07/2014", 13.00f, 5, 10.00f);
-		oProcurado.calculaDesconto("03/07/2000", 11233);
-		System.out.println(oProcurado);
+		try {
+			
+			EntradaDeCinema pokemon = new EntradaDeCinema("03/07/2000", 17.00f,
+					1, 15.00f);
+			pokemon.calculaDesconto("03/07/1988");
+			System.out.println(pokemon);
+
+			EntradaDeCinema oProcurado = new EntradaDeCinema("03/07/2014",
+					13.00f, 5, 10.00f);
+			oProcurado.calculaDesconto("03/07/2000", 11233);
+			System.out.println(oProcurado);
+			
+		} catch (ParseException pe) {
+			pe.printStackTrace();
+		}
 	}
 
 }
---
+++ src/oo/exercicios/util/DataMundoReal.java
@@ -0,0 +1,31 @@
+package oo.exercicios.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DataMundoReal {
+	public static boolean diferencaAnos(String comparador, String aComparar, int limiteAnos) throws ParseException {
+		// Definindo quanto tempo tem a diferença em anos
+		Calendar dtComparador = Calendar.getInstance();
+		Calendar dtAComparar = Calendar.getInstance();
+		
+		// Criando um formatador de datas no formato padrão brasileiro
+		SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		
+		// Transformando as duas datas String em datas de "verdade"
+		dtComparador.setTime(df.parse(comparador));
+		dtAComparar.setTime(df.parse(aComparar));
+		
+		// Adicionando anos a data a comparar
+		dtAComparar.add(Calendar.YEAR, limiteAnos);
+		
+		// Transformando datas em "longs" e comparando quais delas são maiores ou menores
+		if(dtAComparar.getTimeInMillis() > dtComparador.getTimeInMillis()) {
+			return false;
+		}
+		
+		return true;
+	}
+}
